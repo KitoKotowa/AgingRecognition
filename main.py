9e69562e7475d4ff0e1d2437389c8bb69dcf999b
KitoKotowa <chaukiet220803@gmail.com>
2024-03-13 20:45:56 +0700

    Add script

--- main.py
+++ main.py
@@ -1,68 +1,23 @@
-import os
-import time
-import math
-from datetime import datetime
-import numpy as np
-from PIL import Image
+from argparse import ArgumentParser
 
-from keras import Input
+import yaml
+from pytorch_lightning import Trainer
 
-def open_image(name):
-    try:
-        image = Image.open(name)
-        img_arr = np.array(image)
-        #image.show()
-        return img_arr, name
-    except IOError: 
-        print("Cannot open image!")
+from gan_module import AgingGAN
 
-def calculate_age(taken, dob):
-    birth = datetime.fromordinal(max(int(dob) - 366, 1))
-  
-    if birth.month < 7:
-        return taken - birth.year
-    else:
-        return taken - birth.year - 1
-    
-def build_encoder():
-  
-    input_layer = Input(shape = (64, 64, 3))
-  
-    ## 1st Convolutional Block
-    enc = Conv2D(filters = 32, kernel_size = 5, strides = 2, padding = 'same')(input_layer)
-    # enc = BatchNormalization()(enc)
-    enc = LeakyReLU(alpha = 0.2)(enc)
-  
-    ## 2nd Convolutional Block
-    enc = Conv2D(filters = 64, kernel_size = 5, strides = 2, padding = 'same')(enc)
-    enc = BatchNormalization()(enc)
-    enc = LeakyReLU(alpha = 0.2)(enc)
-  
-    ## 3rd Convolutional Block
-    enc = Conv2D(filters = 128, kernel_size = 5, strides = 2, padding = 'same')(enc)
-    enc = BatchNormalization()(enc)
-    enc = LeakyReLU(alpha = 0.2)(enc)
-  
-    ## 4th Convolutional Block
-    enc = Conv2D(filters = 256, kernel_size = 5, strides = 2, padding = 'same')(enc)
-    enc = BatchNormalization()(enc)
-    enc = LeakyReLU(alpha = 0.2)(enc)
-  
-    ## Flatten layer
-    enc = Flatten()(enc)
-  
-    ## 1st Fully Connected Layer
-    enc = Dense(4096)(enc)
-    enc = BatchNormalization()(enc)
-    enc = LeakyReLU(alpha = 0.2)(enc)
-  
-    ## 2nd Fully Connected Layer
-    enc = Dense(100)(enc)
-  
-    ## Create a model
-    model = Model(inputs = [input_layer], outputs = [enc])
-    return model
-    
+parser = ArgumentParser()
+parser.add_argument('--config', default='configs/aging_gan.yaml', help='Config to use for training')
+
+
+def main():
+    args = parser.parse_args()
+    with open(args.config) as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+    print(config)
+    model = AgingGAN(config)
+    trainer = Trainer(max_epochs=config['epochs'], gpus=config['gpus'], auto_scale_batch_size='binsearch')
+    trainer.fit(model)
+
+
 if __name__ == '__main__':
-    img = open_image('input_img/1.jpg')
-    print("Oke")
+    main()
